7333462f4290d3cec5dc5d0e0079f7674c04c210
RuslanPr0g <theruslan.prog@gmail.com>
2021-11-27 15:38:01 +0200

    Thread is cool but tpl is better really

--- TPLPrikoli/Program.cs
+++ TPLPrikoli/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿var t1 = new Task(() => DoVeryImportantWork(1, 3000));
+var t2 = new Task(() => DoVeryImportantWork(2, 4000));
+
+t1.Start();
+t2.Start();
+
+
+
+Console.ReadKey();
+
+static void DoVeryImportantWork(int id, int mills)
+{
+    Console.WriteLine($"{id} started!");
+    Thread.Sleep(mills);
+    Console.WriteLine($"{id} executed!");
+}
