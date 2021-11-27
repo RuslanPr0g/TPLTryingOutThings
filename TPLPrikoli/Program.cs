d7321967c71c163a80c506d4c2ce335247a8cf0f
RuslanPr0g <theruslan.prog@gmail.com>
2021-11-27 15:57:59 +0200

    WaitAll

--- TPLPrikoli/Program.cs
+++ TPLPrikoli/Program.cs
@@ -1,9 +1,14 @@
-﻿Task.Factory.StartNew(() => DoVeryImportantWork(1, 3000)).ContinueWith((prev) => DoSomeOtherVeryImportantWork(1, 4000));
-Task.Factory.StartNew(() => DoVeryImportantWork(2, 4000)).ContinueWith((prev) => DoSomeOtherVeryImportantWork(2, 3500));
-Task.Factory.StartNew(() => DoVeryImportantWork(3, 7000));
+﻿// factory
+var t1 = Task.Factory.StartNew(() => DoVeryImportantWork(1, 3000)).ContinueWith((prev) => DoSomeOtherVeryImportantWork(1, 4000));
+var t2 = Task.Factory.StartNew(() => DoVeryImportantWork(2, 4000)).ContinueWith((prev) => DoSomeOtherVeryImportantWork(2, 3500));
+var t3 = Task.Factory.StartNew(() => DoVeryImportantWork(3, 7000));
 
+// waiting
+var tlist = new List<Task> { t1, t2, t3 };
+Task.WaitAll(tlist.ToArray());
 
-
+// wait for closing in the background
+Console.WriteLine("Press smth pls");
 Console.ReadKey();
 
 static void DoVeryImportantWork(int id, int mills)
