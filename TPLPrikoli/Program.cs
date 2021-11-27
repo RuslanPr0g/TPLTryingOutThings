3ae78522861f78cc42bde286e435278dac75fb7c
RuslanPr0g <theruslan.prog@gmail.com>
2021-11-27 15:46:53 +0200

    ContinueWith

--- TPLPrikoli/Program.cs
+++ TPLPrikoli/Program.cs
@@ -1,5 +1,6 @@
-﻿Task.Factory.StartNew(() => DoVeryImportantWork(1, 3000));
-Task.Factory.StartNew(() => DoVeryImportantWork(2, 4000));
+﻿Task.Factory.StartNew(() => DoVeryImportantWork(1, 3000)).ContinueWith((prev) => DoSomeOtherVeryImportantWork(1, 4000));
+Task.Factory.StartNew(() => DoVeryImportantWork(2, 4000)).ContinueWith((prev) => DoSomeOtherVeryImportantWork(2, 3500));
+Task.Factory.StartNew(() => DoVeryImportantWork(3, 7000));
 
 
 
@@ -10,4 +11,11 @@ static void DoVeryImportantWork(int id, int mills)
     Console.WriteLine($"{id} started!");
     Thread.Sleep(mills);
     Console.WriteLine($"{id} executed!");
+}
+
+static void DoSomeOtherVeryImportantWork(int id, int mills)
+{
+    Console.WriteLine($"{id} more work started!");
+    Thread.Sleep(mills);
+    Console.WriteLine($"{id} more work executed!");
 }
