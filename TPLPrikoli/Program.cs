a3015b0bce6f9a7a6080d52e3a8107f90aefc0e3
RuslanPr0g <theruslan.prog@gmail.com>
2021-11-27 16:04:58 +0200

    Parallel.ForEach

--- TPLPrikoli/Program.cs
+++ TPLPrikoli/Program.cs
@@ -7,6 +7,15 @@ var t3 = Task.Factory.StartNew(() => DoVeryImportantWork(3, 7000));
 var tlist = new List<Task> { t1, t2, t3 };
 Task.WaitAll(tlist.ToArray());
 
+// ForEachParallel
+var intList = new List<int> { 12, 1235, 1, 12312312, 3, 123, 1, 23, 12, 3, 12, 4, 3, 43, 25, 5, 426, 2, 5, 54, 42, 6, 2 };
+
+// Parallel type has built-in WaitAll kinda thing, so that code won't continue untill this method (loop) finishes
+Parallel.ForEach(intList, t =>
+{
+    global::System.Console.WriteLine("Doing parallel: " + t);
+});
+
 // wait for closing in the background
 Console.WriteLine("Press smth pls");
 Console.ReadKey();
